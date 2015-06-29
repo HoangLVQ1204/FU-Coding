ad6488828a1dee619d4703451726f9626a17deb5
canhnht <canhnhtse03860@fpt.edu.vn>
2015-06-28 16:53:19 -0800

    Add a very simple form for submitting question

--- app/routes/question-router.js
+++ app/routes/question-router.js
@@ -6,5 +6,8 @@ router.get('/ask', function(req, res) {
     res.render('post-question.html');
 });
 
+router.post('/ask', function(req, res) {    
+    res.json(req.body);
+});
 
 module.exports = router;
